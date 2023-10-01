f2aeb010ffbe12e033cdb5a27eff9cd7b0a0bfe3
Tyler Wagner <tylerrwagner38@gmail.com>
2023-09-30 19:57:35 -0500

    Initial Commit
    Initial Commit to github to start the project

---
+++ main.cpp
@@ -0,0 +1,16 @@
+//
+// Created by tyler on 9/30/2023.
+//
+
+//all the imports will go here
+#include<string>
+#include<iostream>
+#include "main.h"
+
+using namespace std;
+
+int main()
+{
+
+    return 0;
+}
---
+++ main.h
@@ -0,0 +1,15 @@
+
+//
+// Created by tyler on 9/30/2023.
+//
+
+#ifndef ENGIMA_MACHINE_MAIN_H
+#define ENGIMA_MACHINE_MAIN_H
+
+
+class main {
+
+};
+
+
+#endif //ENGIMA_MACHINE_MAIN_H
